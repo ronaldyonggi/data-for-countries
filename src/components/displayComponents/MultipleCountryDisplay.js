c806be9cd29907e0cc3fd76df352adb766a13d8d
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-15 16:05:04 -0400

    2.19 completed

--- src/components/displayComponents/MultipleCountryDisplay.js
+++ src/components/displayComponents/MultipleCountryDisplay.js
@@ -1,7 +1,16 @@
+import { useState } from "react"
+import DetailedCountryDisplay from "./DetailedCountryDisplay"
+
 const MultipleCountryDisplay = ({country})  => {
+  const [show, setShow] = useState(false)
+
   return (
     <div>
       {country.name.common}
+      <button onClick={() => setShow(!show)}>{show ? 'hide' : 'show'}</button>
+      {show && 
+      <DetailedCountryDisplay country={country} />
+      }
     </div>
   )
 }
